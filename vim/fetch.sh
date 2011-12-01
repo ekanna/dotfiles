a54d20c1c42ab4c57d409bca0207a30606f5642e
Darius Kucinskas <dkucinskas@gmail.com>
2011-12-01 19:01:55 +0200

    Clean up of vimrc file.

--- vim/fetch.sh
+++ vim/fetch.sh
@@ -7,3 +7,6 @@ curl -O https://raw.github.com/jnurmine/Zenburn/master/zenburn.vim
 rm solarized.vim
 curl -O https://raw.github.com/altercation/vim-colors-solarized/master/colors/solarized.vim
 
+rm vividchalk.vim
+curl -O https://raw.github.com/tpope/vim-vividchalk/master/colors/vividchalk.vim
+
