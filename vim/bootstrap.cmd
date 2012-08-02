cbb83d6f990e042215ae2f0298874edcd68e20d2
Darius Kucinskas <d.kucinskas@gmail.com>
2012-08-02 09:24:26 +0300

    Fixed bootstrap.cmd to support 64bit windows.

--- vim/bootstrap.cmd
+++ vim/bootstrap.cmd
@@ -1,3 +1,17 @@
-xcopy /F /Y ".\vimrc" "%programfiles%\vim\_vimrc"
-xcopy /F /Y ".\vim\colors\molokai.vim" "%programfiles%\vim\vimfiles\colors\molokai.vim"
+@echo off
+
+if %PROCESSOR_ARCHITECTURE% == x86 (
+
+	echo f | xcopy /F /Y ".\vimrc" "%PROGRAMFILES%\vim\_vimrc"
+	echo f | xcopy /F /Y ".\vim\colors\molokai.vim" "%PROGRAMFILES%\vim\vimfiles\colors\molokai.vim"
+	goto :eof
+
+) else (
+
+	echo f | xcopy /F /Y ".\vimrc" "%PROGRAMFILES(x86)%\vim\_vimrc"
+	echo f | xcopy /F /Y ".\vim\colors\molokai.vim" "%PROGRAMFILES(x86)%\vim\vimfiles\colors\molokai.vim"
+	goto :eof
+)
+
+
 
